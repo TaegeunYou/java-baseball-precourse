f680c1f005dc04877434a3b7978d25ec792b748a
YouTaegeun <dbxorms2310@naver.com>
2026-01-28 18:56:35 +0900

    refac(OutputView): 출력 로직 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -23,28 +23,31 @@ public class OutputView {
 
     public void printlnStrikeAndBall(Result result) {
         StringBuilder sb = new StringBuilder();
-        appendStrike(result, sb);
-        appendBall(result, sb);
+        appendStrike(result.getStrike(), sb);
+        appendBall(result.getBall(), sb);
+        if (sb.isEmpty()) {
+            printlnMessage("낫싱");
+        }
         printlnMessage(sb.toString());
-    }
-
-    private void appendStrike(Result result, StringBuilder sb) {
-        if (result.getStrike() != 0) {
-            sb.append(result.getStrike()).append("스트라이크");
-        }
-    }
-
-    private void appendBall(Result result, StringBuilder sb) {
-        if (result.getBall() != 0) {
-            if (!sb.isEmpty()) {
-                sb.append(" ");
-            }
-            sb.append(result.getBall()).append("볼");
-        }
     }
 
     public void printlnThreeStrike() {
         printlnMessage("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
         printlnMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private void appendStrike(int strike, StringBuilder sb) {
+        if (strike != 0) {
+            sb.append(strike).append("스트라이크");
+        }
+    }
+
+    private void appendBall(int ball, StringBuilder sb) {
+        if (ball != 0) {
+            if (!sb.isEmpty()) {
+                sb.append(" ");
+            }
+            sb.append(ball).append("볼");
+        }
     }
 }
